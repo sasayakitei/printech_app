b9186b453465f8c9419b3f7adeabd475b5ee247b
sasayakitei (Kotaro Nanjo) <bluepostoffice@gmail.com>
2020-11-24 02:32:53 +0900

    Set default template engine to Slim

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,7 @@ module CollaboApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators.template_engine = :slim
 
     # Don't generate system test files.
     config.generators.system_tests = nil

923b2b27af36508f808a5c40416b2dab9001bf1d
sasayakitei (Kotaro Nanjo) <bluepostoffice@gmail.com>
2020-12-26 10:39:10 +0900

    test: Add validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :shops, dependent: :destroy
 
   validates :nickname, length: { in: 3..16 }
+  validates :first_name,  length: { maximum: 16 }, allow_blank: true, allow_nil: true
+  validates :family_name, length: { maximum: 16 }, allow_blank: true, allow_nil: true
   validates :avatar_url,
     format: {
       with: /\A#{URI::regexp(%w(http https))}\z/,

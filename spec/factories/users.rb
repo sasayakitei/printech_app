fac785f5354ca613bfc55ad6124507dd1b0421aa
sasayakitei (Kotaro Nanjo) <bluepostoffice@gmail.com>
2020-12-26 10:42:10 +0900

    test: Create test data for User model.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    pass = Faker::Internet.password(min_length: 6)
+
+    email                 { Faker::Internet.email }
+    nickname              { Faker::Internet.username(specifier: 3..16) }
+    password              { pass }
+    password_confirmation { pass }
   end
 end

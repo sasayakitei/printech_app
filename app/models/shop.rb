2ad17ee63d8dcc158f2882c17b07c5ca4ce7d51b
sasayakitei (Kotaro Nanjo) <bluepostoffice@gmail.com>
2020-12-26 22:51:54 +0900

    feat: Add validations.

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,28 @@
 class Shop < ApplicationRecord
   belongs_to :user
   has_one :shop_address, dependent: :destroy
+
+  validates :name, length: { maximum: 32 }, null: false
+  validates :description, length: { maximum: 1024 }
+  validates :website_url, 
+            format: {
+              with: /\A#{URI::regexp(%w(http https))}\z/,
+              message: 'URLではありません'
+            },
+            allow_blank: true,
+            allow_nil: true
+  validates :email,
+            format: {
+              with: /\A[^@\s]+@[^@\s]+\z/,
+              message: 'メールアドレスではありません'
+            },
+            allow_blank: true,
+            allow_nil: true
+  validates :phone_number,
+            format: {
+              with: /\A\d{10,11}\z/,
+              message: '電話番号ではありません'
+            },
+            allow_blank: true,
+            allow_nil: true
 end
